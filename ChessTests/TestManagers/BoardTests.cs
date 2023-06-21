2391576d7bdc850e9f286149f2edd7d3ab1b271b
Ecstatik <johankool2003@hotmail.co.uk>
2023-06-21 14:15:00 +0100

    Added tests to cover FEN notation import

--- ChessTests/TestManagers/BoardTests.cs
+++ ChessTests/TestManagers/BoardTests.cs
@@ -11,7 +11,7 @@ namespace ChessTests.TestManagers
         [Fact]
         public void Test_Full_Board_Creation()
         {
-            Assert.Equal(64, board.contents.Count);
+            Assert.Equal(64, board.Contents.Count);
         }
 
         [Fact]
@@ -53,6 +53,40 @@ namespace ChessTests.TestManagers
         {
             board.LoadMoves();
             Assert.Equal(0, board.GetPiece("C1").LegalMoves.Count);
+        }
+
+        [Fact]
+        public void Test_Correct_FEN_Parsing()
+        {
+            string FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+            LogicalBoard board2 = new LogicalBoard(FEN);
+
+            foreach (var content in board.Contents.Keys)
+            {
+                if (board.Contents[content] != null)
+                {
+                    if (board.Contents[content].GetType() != board2.Contents[content].GetType())
+                    {
+                        Assert.False(true);
+                    }
+                }
+
+            }
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test_Invalid_FEN_Parsing_Illegal_Character()
+        {
+            string FEN = "rnbqkbnr/tppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+            Assert.Throws<Exception>(() => new LogicalBoard(FEN));
+        }
+
+        [Fact]
+        public void Test_Invalid_FEN_Parsing_Excessive_Length()
+        {
+            string FEN = "rnbqkbnr/pppppppp/9/8/8/8/PPPPPPPP/RNBQKBNRR";
+            Assert.Throws<Exception>(() => new LogicalBoard(FEN));
         }
     }
 }
