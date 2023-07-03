df66183dd0948847b81900df0e155bfeff83af9b
Ecstatik <johankool2003@hotmail.co.uk>
2023-07-03 17:27:10 +0100

    Added Ability to take both normally and En Passant

--- PieceLibrary/Pieces/Pawn.cs
+++ PieceLibrary/Pieces/Pawn.cs
@@ -32,20 +32,62 @@ namespace PieceLibrary.Pieces
             }
 
             // Normal Pawn Movement
-            currentPosition[1] += colorMultiplier * 1;
+             int[] newPosition = new[] { currentPosition[0], currentPosition[1] + colorMultiplier};
             AddMove(new Move(Square,
-                helper.ConvertToString(currentPosition),
+                helper.ConvertToString(newPosition),
                 this, false));
 
+            // Moving for the first time
             if (firstMove) // The piece can move two squares if it is the first move
             {
-                currentPosition[1] += colorMultiplier * 1;
+                newPosition[1] += colorMultiplier;
                 AddMove(new Move(Square,
-                    helper.ConvertToString(currentPosition),
+                    helper.ConvertToString(newPosition),
                     this, false));
 
             }
 
+            #region Taking Pieces
+
+
+            // To the right
+            List<int[]> newPositionsList = new()
+            { new [] {currentPosition[0] + 1, currentPosition[1] + colorMultiplier}, // Taking forwards
+              new [] {currentPosition[0] - 1, currentPosition[1] + colorMultiplier},
+              new [] {currentPosition[0] + 1, currentPosition[1]}, // Taking En Passant
+              new [] {currentPosition[0] - 1, currentPosition[1]}
+            };
+            Piece? piece;
+            int index = 0;
+            foreach (int[] position in newPositionsList)
+            {
+                index++;
+                if (helper.BoundsCheck(position))
+                {
+                    piece = Board.GetPiece(helper.ConvertToString(position));
+                    if(index <= 2) // Normal Taking
+                    {
+                        if(piece is not null && piece.Color != Color)
+                        {
+                            AddMove(new Move(Square,
+                                helper.ConvertToString(position),
+                                    this, true));
+                        }
+                    }
+                    else // Check En Passant Taking
+                    {
+                        if (piece is not null && firstMove && piece is Pawn && piece.Color != Color)
+                        {
+                            AddMove(new Move(Square,
+                                helper.ConvertToString(position),
+                                    this, true));
+                        }
+                    }
+
+                }
+
+            }
+            #endregion
         }
 
         public override void MovePiece(Move move)
